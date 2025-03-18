0a187d3747433cdc8069fd88a81fb61f3cd7beb4
unknown <chatonode@gmail.com>
2025-03-18 18:44:30 +0300

    feat: move noun json files into this repository with included type property for each word

--- js/urls.js
+++ js/urls.js
@@ -1,13 +1,24 @@
 // URLs for different word lists
+// const JSON_URLS_NOUN = {
+//   b1telcpt1:
+//     'https://script.google.com/macros/s/AKfycbx4gHC-bY0VmvCeZo1fnZ2myu9qdEIoJw0xCk0kjb-yFVinPHypN458aZDdlI1CEJM/exec',
+//   b1telcpt2:
+//     'https://script.googleusercontent.com/macros/echo?user_content_key=Gs_T1_BtJgo68Rq7DUrGxs1uKJH3uaXkCkAAG-VJlJTyeFLEEndTWIwgDxUFlxPsEnbhNpXqL5p-A_udEgZzOavWNpCykLr8m5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnALkkP5rhZLWK4mhfbdLHXykRb2__QFrBFG1jt2RdQ25xS94AGBZe3egWcJgiE_WTDETglXqkmc3b4QFRgnNx1W8cv7vWJ9jUQ&lib=MmEIQx_PXkRyMvxX9OmNoYFLDhaBgl_dE',
+//   b1telcpt3:
+//     'https://script.googleusercontent.com/macros/echo?user_content_key=-DqeJ4hRVzZQ6jg7GzNqXqau-YDeYAuUbIJSfCxdlec8ogcIlFMrDJHGJL_gOhTKjoKxohhDhQeRX8RbXMplgDwM6CyHTGmnm5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnENgxJCDFrjs6JyZ1GGG4RM390WSYlFJahGv7vxAesNVHcnZqINdAEhBXe369Q_rkTCCIDBzZyM04DQEGU-K7khKoSOdad6tfw&lib=MmEIQx_PXkRyMvxX9OmNoYFLDhaBgl_dE',
+//   b1telcpt4:
+//     'https://script.googleusercontent.com/macros/echo?user_content_key=X5GmhOorbYcFAO9AlOBaKeigyKoSTW3fsY_nKBLZH6yNSgHGmqEU7BW-sShIapltxLFruSb5AYWrB4wVzGmkJIPVhIyUWXSYm5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnJZ31fvYy2gsmZkeRqZLyZW-rzexs4eP9SbReR_KHkVIG4bpUFOqrAIxJMj1cgsZLrxKiHKDk848jFZLaVWSAKXiKlrA50qxHQ&lib=MmEIQx_PXkRyMvxX9OmNoYFLDhaBgl_dE',
+// }
+
 const JSON_URLS_NOUN = {
   b1telcpt1:
-    'https://script.google.com/macros/s/AKfycbx4gHC-bY0VmvCeZo1fnZ2myu9qdEIoJw0xCk0kjb-yFVinPHypN458aZDdlI1CEJM/exec',
+    'https://chatonode.github.io/genau-webflow-demo/json/a1-a2/noun.json',
   b1telcpt2:
-    'https://script.googleusercontent.com/macros/echo?user_content_key=Gs_T1_BtJgo68Rq7DUrGxs1uKJH3uaXkCkAAG-VJlJTyeFLEEndTWIwgDxUFlxPsEnbhNpXqL5p-A_udEgZzOavWNpCykLr8m5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnALkkP5rhZLWK4mhfbdLHXykRb2__QFrBFG1jt2RdQ25xS94AGBZe3egWcJgiE_WTDETglXqkmc3b4QFRgnNx1W8cv7vWJ9jUQ&lib=MmEIQx_PXkRyMvxX9OmNoYFLDhaBgl_dE',
+    'https://chatonode.github.io/genau-webflow-demo/json/a2-b1/noun.json',
   b1telcpt3:
-    'https://script.googleusercontent.com/macros/echo?user_content_key=-DqeJ4hRVzZQ6jg7GzNqXqau-YDeYAuUbIJSfCxdlec8ogcIlFMrDJHGJL_gOhTKjoKxohhDhQeRX8RbXMplgDwM6CyHTGmnm5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnENgxJCDFrjs6JyZ1GGG4RM390WSYlFJahGv7vxAesNVHcnZqINdAEhBXe369Q_rkTCCIDBzZyM04DQEGU-K7khKoSOdad6tfw&lib=MmEIQx_PXkRyMvxX9OmNoYFLDhaBgl_dE',
+    'https://chatonode.github.io/genau-webflow-demo/json/b1-b2/noun.json',
   b1telcpt4:
-    'https://script.googleusercontent.com/macros/echo?user_content_key=X5GmhOorbYcFAO9AlOBaKeigyKoSTW3fsY_nKBLZH6yNSgHGmqEU7BW-sShIapltxLFruSb5AYWrB4wVzGmkJIPVhIyUWXSYm5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnJZ31fvYy2gsmZkeRqZLyZW-rzexs4eP9SbReR_KHkVIG4bpUFOqrAIxJMj1cgsZLrxKiHKDk848jFZLaVWSAKXiKlrA50qxHQ&lib=MmEIQx_PXkRyMvxX9OmNoYFLDhaBgl_dE',
+    'https://chatonode.github.io/genau-webflow-demo/json/c1-c2/noun.json',
 }
 
 const JSON_URLS_VERB = {
