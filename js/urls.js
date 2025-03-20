1af2f3de6b6479ff18d8a6d9185140be96ca09aa
unknown <chatonode@gmail.com>
2025-03-21 00:24:57 +0300

    revert: verb word urls and static data from test to its normal version

--- js/urls.js
+++ js/urls.js
@@ -23,8 +23,7 @@ const JSON_URLS_NOUN = {
 
 const JSON_URLS_VERB = {
   b1telcpt1:
-    // 'https://chatonode.github.io/genau-webflow-demo/json/a1-a2/verb.json',
-    'https://chatonode.github.io/genau-webflow-demo/json/test/verb-laura.json',
+    'https://chatonode.github.io/genau-webflow-demo/json/a1-a2/verb.json',
   b1telcpt2:
     'https://chatonode.github.io/genau-webflow-demo/json/a2-b1/verb.json',
   b1telcpt3:
