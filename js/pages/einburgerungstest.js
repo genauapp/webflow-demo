ad228566747034f5c9dc8863c286f414925eed65
unknown <chatonode@gmail.com>
2025-03-24 14:44:45 +0300

    feat: load questions from json

--- js/pages/einburgerungstest.js
+++ js/pages/einburgerungstest.js
@@ -3,15 +3,35 @@ import {
   getQuestionsByState,
 } from '../utils/einbürgerungstestUtils.js'
 
-let selectedState
+import questionsJson from '../json/einbürgerungstest' with { type: 'json' }
+
+let currentState
+
+const questions = []
+
+const loadQuestionsJson = async () => {
+  return getQuestionsByState(questionsJson, currentState)
+}
 
 document.querySelectorAll('.state-dropdown-link').forEach((link) => {
   link.addEventListener('click', async function (event) {
-    event.preventDefault()
-    const selectedState = link.getAttribute('data-option') || 'Berlin'
+    // event.preventDefault()
+    const currentState = link.getAttribute('data-option')
 
-    document.getElementById('dropdown-header').innerText = selectedState
+    document.getElementById('dropdown-header').innerText = currentState
 
-    console.log(selectedState)
+    console.log(currentState)
+
+    await loadQuestionsJson()
   })
+})
+
+document.addEventListener('DOMContentLoaded', async () => {
+  currentState = "Berlin"
+
+  const loadedQuestions = await loadQuestionsJson()
+
+  questions = [...loadedQuestions]
+
+  console.log(questions)
 })
