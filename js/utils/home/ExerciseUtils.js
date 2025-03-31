00810a466c7933e317ff771ed9de6ad9370b31f2
unknown <chatonode@gmail.com>
2025-03-31 20:25:12 +0100

    refactor: introduce exercise and number utils to move code from home.js to separate files

---
+++ js/utils/home/ExerciseUtils.js
@@ -0,0 +1,35 @@
+import NumberUtils from '../NumberUtils.js'
+
+export default class ExerciseUtils {
+  static shouldUseOwnMeaning = () => {
+    // Math.random() returns a number between 0 (inclusive) and 1 (exclusive).
+    // If the number is less than 0.6, that's a 60% chance.
+    const useOwnMeaning = Math.random() < 0.6
+    console.log(
+      'decided as: ' + (useOwnMeaning ? 'own meaning' : 'different meaning')
+    )
+    return useOwnMeaning
+  }
+
+  static getRandomTranslationResult = (selectedWord, staticWordLists) => {
+    const kelimeListesiInstance = staticWordLists[currentLevel][currentType]
+    const filteredKelimeListesiExercise = kelimeListesiInstance.filter(
+      (kelimeExercise) => kelimeExercise.almanca !== selectedWord.almanca
+    )
+
+    console.log('kelime listesi exercise Instance:')
+    console.log(kelimeListesiInstance)
+    console.log('filtered kelime listesi exercise:')
+    console.log(filteredKelimeListesiExercise)
+
+    const randomIndex = NumberUtils.getRandomNumber(
+      filteredKelimeListesiExercise.length - 1
+    )
+
+    console.log(filteredKelimeListesiExercise[randomIndex])
+
+    const randomResult = filteredKelimeListesiExercise[randomIndex].ingilizce
+
+    return randomResult
+  }
+}
