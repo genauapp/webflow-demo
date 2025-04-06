d09c2862370a5eedcff6500514f16a6ed9576bd2
s2jack <gercekselcukguler@gmail.com>
2025-04-06 23:02:59 +0300

    create folders for each category for every level & declare them on urls.js

--- js/constants/urls.js
+++ js/constants/urls.js
@@ -1,15 +1,3 @@
-// URLs for different word lists
-// const JSON_URLS_NOUN = {
-//   b1telcpt1:
-//     'https://script.google.com/macros/s/AKfycbx4gHC-bY0VmvCeZo1fnZ2myu9qdEIoJw0xCk0kjb-yFVinPHypN458aZDdlI1CEJM/exec',
-//   b1telcpt2:
-//     'https://script.googleusercontent.com/macros/echo?user_content_key=Gs_T1_BtJgo68Rq7DUrGxs1uKJH3uaXkCkAAG-VJlJTyeFLEEndTWIwgDxUFlxPsEnbhNpXqL5p-A_udEgZzOavWNpCykLr8m5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnALkkP5rhZLWK4mhfbdLHXykRb2__QFrBFG1jt2RdQ25xS94AGBZe3egWcJgiE_WTDETglXqkmc3b4QFRgnNx1W8cv7vWJ9jUQ&lib=MmEIQx_PXkRyMvxX9OmNoYFLDhaBgl_dE',
-//   b1telcpt3:
-//     'https://script.googleusercontent.com/macros/echo?user_content_key=-DqeJ4hRVzZQ6jg7GzNqXqau-YDeYAuUbIJSfCxdlec8ogcIlFMrDJHGJL_gOhTKjoKxohhDhQeRX8RbXMplgDwM6CyHTGmnm5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnENgxJCDFrjs6JyZ1GGG4RM390WSYlFJahGv7vxAesNVHcnZqINdAEhBXe369Q_rkTCCIDBzZyM04DQEGU-K7khKoSOdad6tfw&lib=MmEIQx_PXkRyMvxX9OmNoYFLDhaBgl_dE',
-//   b1telcpt4:
-//     'https://script.googleusercontent.com/macros/echo?user_content_key=X5GmhOorbYcFAO9AlOBaKeigyKoSTW3fsY_nKBLZH6yNSgHGmqEU7BW-sShIapltxLFruSb5AYWrB4wVzGmkJIPVhIyUWXSYm5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnJZ31fvYy2gsmZkeRqZLyZW-rzexs4eP9SbReR_KHkVIG4bpUFOqrAIxJMj1cgsZLrxKiHKDk848jFZLaVWSAKXiKlrA50qxHQ&lib=MmEIQx_PXkRyMvxX9OmNoYFLDhaBgl_dE',
-// }
-
 // PROD
 export const ASSETS_BASE_URL = `https://genauapp.github.io/webflow-demo/`
 // STAGING
@@ -19,34 +7,130 @@ export const ASSETS_BASE_URL = `https://genauapp.github.io/webflow-demo/`
 /* WORD LEARN/EXERCISE **/
 
 const JSON_URLS_NOUN = {
-  b1telcpt1: `${ASSETS_BASE_URL}/json/a1-a2/noun.json`,
-  b1telcpt2: `${ASSETS_BASE_URL}/json/a2-b1/noun.json`,
-  b1telcpt3: `${ASSETS_BASE_URL}/json/b1-b2/noun.json`,
-  b1telcpt4: `${ASSETS_BASE_URL}/json/c1-c2/noun.json`,
+  b1telcpt1: 
+  {
+    default: `${ASSETS_BASE_URL}/json/a1-a2/default/noun.json`,
+    dirty: `${ASSETS_BASE_URL}/json/a1-a2/dirty/noun.json`,
+    doctor: `${ASSETS_BASE_URL}/json/a1-a2/doctor/noun.json`,
+    music: `${ASSETS_BASE_URL}/json/a1-a2/music/noun.json`
+  },
+  b1telcpt2:
+  {
+    default: `${ASSETS_BASE_URL}/json/a2-b1/default/noun.json`,
+    dirty: `${ASSETS_BASE_URL}/json/a2-b1/dirty/noun.json`,
+    doctor: `${ASSETS_BASE_URL}/json/a2-b1/doctor/noun.json`,
+    music: `${ASSETS_BASE_URL}/json/a2-b1/music/noun.json`
+  },
+  b1telcpt3:
+  {
+    default: `${ASSETS_BASE_URL}/json/b1-b2/default/noun.json`,
+    dirty: `${ASSETS_BASE_URL}/json/b1-b2/dirty/noun.json`,
+    doctor: `${ASSETS_BASE_URL}/json/b1-b2/doctor/noun.json`,
+    music: `${ASSETS_BASE_URL}/json/b1-b2/music/noun.json`
+  },
+  b1telcpt4:
+  {
+    default: `${ASSETS_BASE_URL}/json/c1-c2/default/noun.json`,
+    dirty: `${ASSETS_BASE_URL}/json/c1-c2/dirty/noun.json`,
+    doctor: `${ASSETS_BASE_URL}/json/c1-c2/doctor/noun.json`,
+    music: `${ASSETS_BASE_URL}/json/c1-c2/music/noun.json`
+  },
   einburgerungstest: `${ASSETS_BASE_URL}/json/einburgerungstest/noun.json`,
 }
 
 const JSON_URLS_VERB = {
-  b1telcpt1: `${ASSETS_BASE_URL}/json/a1-a2/verb.json`,
-  b1telcpt2: `${ASSETS_BASE_URL}/json/a2-b1/verb.json`,
-  b1telcpt3: `${ASSETS_BASE_URL}/json/b1-b2/verb.json`,
-  b1telcpt4: `${ASSETS_BASE_URL}/json/c1-c2/verb.json`,
+  b1telcpt1: 
+  {
+    default: `${ASSETS_BASE_URL}/json/a1-a2/default/verb.json`,
+    dirty: `${ASSETS_BASE_URL}/json/a1-a2/dirty/verb.json`,
+    doctor: `${ASSETS_BASE_URL}/json/a1-a2/doctor/verb.json`,
+    music: `${ASSETS_BASE_URL}/json/a1-a2/music/verb.json`
+  },
+  b1telcpt2:
+  {
+    default: `${ASSETS_BASE_URL}/json/a2-b1/default/verb.json`,
+    dirty: `${ASSETS_BASE_URL}/json/a2-b1/dirty/verb.json`,
+    doctor: `${ASSETS_BASE_URL}/json/a2-b1/doctor/verb.json`,
+    music: `${ASSETS_BASE_URL}/json/a2-b1/music/verb.json`
+  },
+  b1telcpt3:
+  {
+    default: `${ASSETS_BASE_URL}/json/b1-b2/default/verb.json`,
+    dirty: `${ASSETS_BASE_URL}/json/b1-b2/dirty/verb.json`,
+    doctor: `${ASSETS_BASE_URL}/json/b1-b2/doctor/verb.json`,
+    music: `${ASSETS_BASE_URL}/json/b1-b2/music/verb.json`
+  },
+  b1telcpt4:
+  {
+    default: `${ASSETS_BASE_URL}/json/c1-c2/default/verb.json`,
+    dirty: `${ASSETS_BASE_URL}/json/c1-c2/dirty/verb.json`,
+    doctor: `${ASSETS_BASE_URL}/json/c1-c2/doctor/verb.json`,
+    music: `${ASSETS_BASE_URL}/json/c1-c2/music/verb.json`,
+  },
   einburgerungstest: `${ASSETS_BASE_URL}/json/einburgerungstest/verb.json`,
 }
 
 const JSON_URLS_ADJECTIVE = {
-  b1telcpt1: `${ASSETS_BASE_URL}/json/a1-a2/adjective.json`,
-  b1telcpt2: `${ASSETS_BASE_URL}/json/a2-b1/adjective.json`,
-  b1telcpt3: `${ASSETS_BASE_URL}/json/b1-b2/adjective.json`,
-  b1telcpt4: `${ASSETS_BASE_URL}/json/c1-c2/adjective.json`,
+  b1telcpt1: 
+  {
+    default: `${ASSETS_BASE_URL}/json/a1-a2/default/adjective.json`,
+    dirty: `${ASSETS_BASE_URL}/json/a1-a2/dirty/adjective.json`,
+    doctor: `${ASSETS_BASE_URL}/json/a1-a2/doctor/adjective.json`,
+    music: `${ASSETS_BASE_URL}/json/a1-a2/music/adjective.json`
+  },
+  b1telcpt2:
+  {
+    default: `${ASSETS_BASE_URL}/json/a2-b1/default/adjective.json`,
+    dirty: `${ASSETS_BASE_URL}/json/a2-b1/dirty/adjective.json`,
+    doctor: `${ASSETS_BASE_URL}/json/a2-b1/doctor/adjective.json`,
+    music: `${ASSETS_BASE_URL}/json/a2-b1/music/adjective.json`
+  },
+  b1telcpt3:
+  {
+    default: `${ASSETS_BASE_URL}/json/b1-b2/default/adjective.json`,
+    dirty: `${ASSETS_BASE_URL}/json/b1-b2/dirty/adjective.json`,
+    doctor: `${ASSETS_BASE_URL}/json/b1-b2/doctor/adjective.json`,
+    music: `${ASSETS_BASE_URL}/json/b1-b2/music/adjective.json`
+  },
+  b1telcpt4:
+  {
+    default: `${ASSETS_BASE_URL}/json/c1-c2/default/adjective.json`,
+    dirty: `${ASSETS_BASE_URL}/json/c1-c2/dirty/adjective.json`,
+    doctor: `${ASSETS_BASE_URL}/json/c1-c2/doctor/adjective.json`,
+    music: `${ASSETS_BASE_URL}/json/c1-c2/music/adjective.json`,
+  },
   einburgerungstest: `${ASSETS_BASE_URL}/json/einburgerungstest/adjective.json`,
 }
 
 const JSON_URLS_ADVERB = {
-  b1telcpt1: `${ASSETS_BASE_URL}/json/a1-a2/adverb.json`,
-  b1telcpt2: `${ASSETS_BASE_URL}/json/a2-b1/adverb.json`,
-  b1telcpt3: `${ASSETS_BASE_URL}/json/b1-b2/adverb.json`,
-  b1telcpt4: `${ASSETS_BASE_URL}/json/c1-c2/adverb.json`,
+  b1telcpt1: 
+  {
+    default: `${ASSETS_BASE_URL}/json/a1-a2/default/adverb.json`,
+    dirty: `${ASSETS_BASE_URL}/json/a1-a2/dirty/adverb.json`,
+    doctor: `${ASSETS_BASE_URL}/json/a1-a2/doctor/adverb.json`,
+    music: `${ASSETS_BASE_URL}/json/a1-a2/music/adverb.json`
+  },
+  b1telcpt2:
+  {
+    default: `${ASSETS_BASE_URL}/json/a2-b1/default/adverb.json`,
+    dirty: `${ASSETS_BASE_URL}/json/a2-b1/dirty/adverb.json`,
+    doctor: `${ASSETS_BASE_URL}/json/a2-b1/doctor/adverb.json`,
+    music: `${ASSETS_BASE_URL}/json/a2-b1/music/adverb.json`
+  },
+  b1telcpt3:
+  {
+    default: `${ASSETS_BASE_URL}/json/b1-b2/default/adverb.json`,
+    dirty: `${ASSETS_BASE_URL}/json/b1-b2/dirty/adverb.json`,
+    doctor: `${ASSETS_BASE_URL}/json/b1-b2/doctor/adverb.json`,
+    music: `${ASSETS_BASE_URL}/json/b1-b2/music/adverb.json`
+  },
+  b1telcpt4:
+  {
+    default: `${ASSETS_BASE_URL}/json/c1-c2/default/adverb.json`,
+    dirty: `${ASSETS_BASE_URL}/json/c1-c2/dirty/adverb.json`,
+    doctor: `${ASSETS_BASE_URL}/json/c1-c2/doctor/adverb.json`,
+    music: `${ASSETS_BASE_URL}/json/c1-c2/music/adverb.json`,
+  },
   einburgerungstest: `${ASSETS_BASE_URL}/json/einburgerungstest/adverb.json`,
 }
 
