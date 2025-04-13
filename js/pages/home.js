8a7b2dda2003590404ed155443d09db8032a7fe0
s2jack <gercekselcukguler@gmail.com>
2025-04-14 00:58:57 +0300

    import updateFavoriteIcons function into showLearnWords.js file

--- js/pages/home.js
+++ js/pages/home.js
@@ -382,107 +382,6 @@ function artikelRenk(artikel) {
   return 'black'
 }
 
-function showLearnWord() {
-  const level = LocalStorageManager.load(CURRENT_LEVEL_KEY, DEFAULT_VALUE.CURRENT_LEVEL)
-  const wordType = LocalStorageManager.load(CURRENT_WORD_TYPE_KEY, DEFAULT_VALUE.CURRENT_WORD_TYPE)
-  const category = LocalStorageManager.load(CURRENT_CATEGORY_KEY, DEFAULT_VALUE.CURRENT_CATEGORY)
-  const learnedWithLearnWords = LocalStorageManager.load(LEARNED_WITH_LEARN_WORDS_KEY, DEFAULT_VALUE.LEARNED_WITH_LEARN_WORDS)
-  const iKnowButton = document.getElementById(
-    `iKnowButtonLearn-${wordType}`
-  )
-  const repeatButton = document.getElementById(
-    `repeatButtonLearn-${wordType}`
-  )
-
-  if (learnedWithLearnWords[level][category][wordType].length === totalWordsLearn) {
-    document.getElementById(`wordLearn-${wordType}`).innerText =
-      'No words to display.'
-    document.getElementById(`translationLearn-${wordType}`).innerText = ''
-    document.getElementById(`exampleLearn-${wordType}`).innerText = ''
-    document.getElementById(`levelTagLearn-${wordType}`).innerText = ''
-    document.getElementById(`ruleLearn-${wordType}`).innerText = '' // Kural boş
-
-    if (iKnowButton) {
-      iKnowButton.style.visibility = 'hidden'
-    }
-    if (repeatButton) {
-      repeatButton.style.visibility = 'hidden'
-    }
-    return
-  }
-  // else
-  // // reactivate buttons
-  iKnowButton.style.visibility = 'visible'
-  repeatButton.style.visibility = 'visible'
-
-  const { almanca, ingilizce, ornek, highlight, seviye, kural } =
-    wordList[currentLearnIndex]
-
-  if (learnedWithLearnWords[level][category][wordType].length > 0) {
-    wordList = wordList.filter(
-      (word) =>
-        !learnedWithLearnWords[level][category][wordType].some(
-          (learned) => learned.almanca === word.almanca
-        )
-    )
-  }
-
-  switch (wordType) {
-    case 'noun':
-      // Highlight kısmını vurgula
-      let highlightedWord = almanca
-      if (highlight) {
-        const regex = new RegExp(`(${highlight})`, 'i')
-        highlightedWord = almanca.replace(
-          regex,
-          `<span class="highlight">$1</span>`
-        )
-      }
-      const renk = artikelRenk(wordList[currentLearnIndex].artikel)
-      document.getElementById(
-        'wordLearn-' + wordType
-      ).innerHTML = `<span style="color: ${renk};">${highlightedWord}</span>`
-      break
-    case 'verb':
-      document.getElementById('wordLearn-' + wordType).innerHTML = almanca
-      break
-    case 'adjective':
-      document.getElementById('wordLearn-' + wordType).innerHTML = almanca
-      break
-    case 'adverb':
-      document.getElementById('wordLearn-' + wordType).innerHTML = almanca
-      break
-  }
-
-  document.getElementById(`levelTagLearn-${wordType}`).innerText =
-    seviye || 'N/A'
-  document.getElementById('translationLearn-' + wordType).innerText =
-    ingilizce || 'N/A'
-  document.getElementById('exampleLearn-' + wordType).innerText =
-    ornek || 'N/A'
-
-  const ruleLearnElement = document.getElementById('ruleLearn-' + wordType)
-  const isAdjectiveOrAdverb =
-    wordType === 'adjective' || wordType === 'adverb'
-
-  // Kural setini göster
-  if (!kural || isAdjectiveOrAdverb) {
-    ruleLearnElement.innerText = ''
-    ruleLearnElement.style.display = 'none'
-  } else {
-    ruleLearnElement.innerText = `${kural}`
-    ruleLearnElement.style.display = 'block'
-
-    // Animasyonu tekrar ettir
-    ruleLearnElement.classList.remove('highlight-animation')
-    void ruleLearnElement.offsetWidth // Bu satır animasyonu yeniden tetikler
-    ruleLearnElement.classList.add('highlight-animation')
-  }
-
-  // Favori ikonlarını güncelle
-  updateFavoriteIcons(wordType)
-}
-
 export const nounDerAnswerClickHandler = function (event) {
   event.preventDefault() // Sayfanın yukarı kaymasını engeller
   checkNounAnswer('der')
@@ -719,7 +618,7 @@ function isItInFavorites(currentWord, favoriteWords) {
   return favoriteWords.some((word) => word.almanca === currentWord.almanca)
 }
 
-function updateFavoriteIcons(wordType) {
+export function updateFavoriteIcons(wordType) {
   const inFavImage = document.getElementById(`infav-${wordType}`)
   const outFavImage = document.getElementById(`outfav-${wordType}`)
 
---
+++ js/utils/home/showLearnWord.js
@@ -0,0 +1,106 @@
+import { DEFAULT_VALUE, WORD_LIST_KEY, CURRENT_CATEGORY_KEY, CURRENT_LEVEL_KEY, CURRENT_WORD_TYPE_KEY, LEARNED_WITH_LEARN_WORDS_KEY } from "../../constants/storageKeys.js"
+import LocalStorageManager from "../LocalStorageManager.js"
+import { updateFavoriteIcons } from "../../pages/home.js"
+
+export default function showLearnWord(currentLearnIndex) {
+    const level = LocalStorageManager.load(CURRENT_LEVEL_KEY, DEFAULT_VALUE.CURRENT_LEVEL)
+    const wordType = LocalStorageManager.load(CURRENT_WORD_TYPE_KEY, DEFAULT_VALUE.CURRENT_WORD_TYPE)
+    const category = LocalStorageManager.load(CURRENT_CATEGORY_KEY, DEFAULT_VALUE.CURRENT_CATEGORY)
+    const learnedWithLearnWords = LocalStorageManager.load(LEARNED_WITH_LEARN_WORDS_KEY, DEFAULT_VALUE.LEARNED_WITH_LEARN_WORDS)
+    const wordList = LocalStorageManager.load(WORD_LIST_KEY, DEFAULT_VALUE.WORD_LIST)
+    
+    const iKnowButton = document.getElementById(
+        `iKnowButtonLearn-${wordType}`
+    )
+    const repeatButton = document.getElementById(
+        `repeatButtonLearn-${wordType}`
+    )
+
+    if (learnedWithLearnWords[level][category][wordType].length === wordList.length) {
+        document.getElementById(`wordLearn-${wordType}`).innerText =
+            'No words to display.'
+        document.getElementById(`translationLearn-${wordType}`).innerText = ''
+        document.getElementById(`exampleLearn-${wordType}`).innerText = ''
+        document.getElementById(`levelTagLearn-${wordType}`).innerText = ''
+        document.getElementById(`ruleLearn-${wordType}`).innerText = '' // Kural boş
+
+        if (iKnowButton) {
+            iKnowButton.style.visibility = 'hidden'
+        }
+        if (repeatButton) {
+            repeatButton.style.visibility = 'hidden'
+        }
+        return
+    }
+    // else
+    // // reactivate buttons
+    iKnowButton.style.visibility = 'visible'
+    repeatButton.style.visibility = 'visible'
+
+    const { almanca, ingilizce, ornek, highlight, seviye, kural } =
+        wordList[currentLearnIndex]
+
+    if (learnedWithLearnWords[level][category][wordType].length > 0) {
+        wordList = wordList.filter(
+            (word) =>
+                !learnedWithLearnWords[level][category][wordType].some(
+                    (learned) => learned.almanca === word.almanca
+                )
+        )
+    }
+
+    switch (wordType) {
+        case 'noun':
+            // Highlight kısmını vurgula
+            let highlightedWord = almanca
+            if (highlight) {
+                const regex = new RegExp(`(${highlight})`, 'i')
+                highlightedWord = almanca.replace(
+                    regex,
+                    `<span class="highlight">$1</span>`
+                )
+            }
+            const renk = artikelRenk(wordList[currentLearnIndex].artikel)
+            document.getElementById(
+                'wordLearn-' + wordType
+            ).innerHTML = `<span style="color: ${renk};">${highlightedWord}</span>`
+            break
+        case 'verb':
+            document.getElementById('wordLearn-' + wordType).innerHTML = almanca
+            break
+        case 'adjective':
+            document.getElementById('wordLearn-' + wordType).innerHTML = almanca
+            break
+        case 'adverb':
+            document.getElementById('wordLearn-' + wordType).innerHTML = almanca
+            break
+    }
+
+    document.getElementById(`levelTagLearn-${wordType}`).innerText =
+        seviye || 'N/A'
+    document.getElementById('translationLearn-' + wordType).innerText =
+        ingilizce || 'N/A'
+    document.getElementById('exampleLearn-' + wordType).innerText =
+        ornek || 'N/A'
+
+    const ruleLearnElement = document.getElementById('ruleLearn-' + wordType)
+    const isAdjectiveOrAdverb =
+        wordType === 'adjective' || wordType === 'adverb'
+
+    // Kural setini göster
+    if (!kural || isAdjectiveOrAdverb) {
+        ruleLearnElement.innerText = ''
+        ruleLearnElement.style.display = 'none'
+    } else {
+        ruleLearnElement.innerText = `${kural}`
+        ruleLearnElement.style.display = 'block'
+
+        // Animasyonu tekrar ettir
+        ruleLearnElement.classList.remove('highlight-animation')
+        void ruleLearnElement.offsetWidth // Bu satır animasyonu yeniden tetikler
+        ruleLearnElement.classList.add('highlight-animation')
+    }
+
+    // Favori ikonlarını güncelle
+    updateFavoriteIcons(wordType)
+}
