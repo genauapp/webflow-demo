0bb55070467c05e8df30ff1e278fb303c6a8e922
s2jack <gercekselcukguler@gmail.com>
2025-04-17 22:39:27 +0300

    change base asset url

--- js/constants/urls.js
+++ js/constants/urls.js
@@ -2,6 +2,6 @@
 //export const ASSETS_BASE_URL = `https://genauapp.github.io/webflow-demo/`
 // STAGING
 export const ASSETS_BASE_URL =
-   'https://cdn.jsdelivr.net/gh/genauapp/webflow-demo@7669c39'
+   'https://cdn.jsdelivr.net/gh/genauapp/webflow-demo@a1529de'
 
 /* WORD LEARN/EXERCISE **/
