14944b43c59bb9fa3f9d58a80ac668826b45d202
s2jack <gercekselcukguler@gmail.com>
2025-04-17 20:41:14 +0300

    improve categories structure in props.js

--- js/constants/props.js
+++ js/constants/props.js
@@ -1,9 +1,69 @@
 export const levels = ['a1', 'a2', 'b1', 'b2', 'c1', 'c2', 'einburgerungstest']
 export const types = ['noun', 'verb', 'adjective', 'adverb']
 export const categories = {
-    a1: ["alltag", "einkaufen", "gesundheit", "behoerden"],
-    a2: ["alltag", "freizeit", "medien", "arbeit"],
-    b1: ["gefühle", "reisen", "wohnen", "slang"],
+    a1: [{
+        nameShort: "alltag",
+        name: "Alltag",
+        nameEng: "Daily Life",
+        imgUrl: "",
+    }, {
+        nameShort: "einkaufen",
+        name: "Einkaufen & Essen",
+        nameEng: "Shopping & Food",
+        imgUrl: "",
+    }, {
+        nameShort: "gesundheit",
+        name: "Gesundheit",
+        nameEng: "Health",
+        imgUrl: "",
+    }, {
+        nameShort: "behoerden",
+        name: "Behörden & Termin",
+        nameEng: "Official Appointments",
+        imgUrl: "",
+    }],
+    a2: [{
+        nameShort: "alltag",
+        name: "Alltag",
+        nameEng: "Daily Life",
+        imgUrl: "",
+    }, {
+        nameShort: "freizeit",
+        name: "Freizeit & Konsum",
+        nameEng: "Leisure & Consumption",
+        imgUrl: "",
+    }, {
+        nameShort: "medien",
+        name: "Medien & Kommunikation",
+        nameEng: "Media & Communication",
+        imgUrl: "",
+    }, {
+        nameShort: "arbeit",
+        name: "Arbeit & Behörden",
+        nameEng: "Work & Authorities",
+        imgUrl: "",
+    }],
+    b1: [{
+        nameShort: "gwfuehle",
+        name: "Gefühle & Beziehungen",
+        nameEng: "Emotions & Relationships",
+        imgUrl: "",
+    }, {
+        nameShort: "reisen",
+        name: "Reisen & Verkehr",
+        nameEng: "Travel & Transportation",
+        imgUrl: "",
+    }, {
+        nameShort: "wohnen",
+        name: "Wohnen & Umwelt",
+        nameEng: "Living & Environment",
+        imgUrl: "",
+    }, {
+        nameShort: "slang",
+        name: "German Slang",
+        nameEng: "German Slang",
+        imgUrl: "",
+    }],
     b2: [],
     c1: [],
     c2: []
