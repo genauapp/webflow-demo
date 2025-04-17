916ed431f03c56b1083bb269217a426e1094a6cb
s2jack <gercekselcukguler@gmail.com>
2025-04-17 22:16:03 +0300

    change assets base url with branch code

--- js/constants/urls.js
+++ js/constants/urls.js
@@ -2,6 +2,6 @@
 //export const ASSETS_BASE_URL = `https://genauapp.github.io/webflow-demo/`
 // STAGING
 export const ASSETS_BASE_URL =
-   'https://cdn.jsdelivr.net/gh/genauapp/webflow-demo@77d97f8'
+   'https://cdn.jsdelivr.net/gh/genauapp/webflow-demo@7669c39'
 
 /* WORD LEARN/EXERCISE **/
