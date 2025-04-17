f7133594b552a965a5b4565cea07770b79caeb1a
s2jack <gercekselcukguler@gmail.com>
2025-04-17 23:42:01 +0300

    update branch url

--- js/constants/urls.js
+++ js/constants/urls.js
@@ -2,6 +2,6 @@
 //export const ASSETS_BASE_URL = `https://genauapp.github.io/webflow-demo/`
 // STAGING
 export const ASSETS_BASE_URL =
-   'https://cdn.jsdelivr.net/gh/genauapp/webflow-demo@a1529de'
+   'https://cdn.jsdelivr.net/gh/genauapp/webflow-demo@270f697'
 
 /* WORD LEARN/EXERCISE **/
