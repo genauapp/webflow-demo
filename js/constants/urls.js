327fb9d5d27cf141a0352f2365610bc8c35d2a36
s2jack <gercekselcukguler@gmail.com>
2025-04-19 14:37:05 +0300

    update assets url

--- js/constants/urls.js
+++ js/constants/urls.js
@@ -2,6 +2,6 @@
 //export const ASSETS_BASE_URL = `https://genauapp.github.io/webflow-demo/`
 // STAGING
 export const ASSETS_BASE_URL =
-   'https://cdn.jsdelivr.net/gh/genauapp/webflow-demo@270f697'
+   'https://cdn.jsdelivr.net/gh/genauapp/webflow-demo@fe06c4e'
 
 /* WORD LEARN/EXERCISE **/
