14362e6a648ba09160a8403f672c1cc9e00a0fb0
unknown <chatonode@gmail.com>
2025-05-06 17:04:09 +0100

    feat: update assets base url commit id, for getting fresh json files

--- js/constants/urls.js
+++ js/constants/urls.js
@@ -2,6 +2,6 @@
 
 //export const ASSETS_BASE_URL = `https://genauapp.github.io/webflow-demo/`
 // STAGING
-export const ASSETS_BASE_URL = 'https://cdn.jsdelivr.net/gh/genauapp/webflow-demo@feefa14'
+export const ASSETS_BASE_URL = 'https://cdn.jsdelivr.net/gh/genauapp/webflow-demo@v8dcf59f'
 
 /* WORD LEARN/EXERCISE **/
