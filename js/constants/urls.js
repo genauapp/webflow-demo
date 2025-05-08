fe32eae631c5ddccd5288eb89ae5bea9c64dee50
unknown <chatonode@gmail.com>
2025-05-08 12:09:48 +0100

    feat: prepare app for main merge/prod publish

--- js/constants/urls.js
+++ js/constants/urls.js
@@ -1,7 +1,6 @@
 // PROD
-
-//export const ASSETS_BASE_URL = `https://genauapp.github.io/webflow-demo/`
+export const ASSETS_BASE_URL = `https://genauapp.github.io/webflow-demo/`
 // STAGING
-export const ASSETS_BASE_URL = 'https://cdn.jsdelivr.net/gh/genauapp/webflow-demo@v8dcf59f'
+// export const ASSETS_BASE_URL = 'https://cdn.jsdelivr.net/gh/genauapp/webflow-demo@v8dcf59f'
 
 /* WORD LEARN/EXERCISE **/
