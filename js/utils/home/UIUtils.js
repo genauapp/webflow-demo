0cb349fa72e9a40253ec5220f6fe91665ad41e6b
unknown <chatonode@gmail.com>
2025-05-09 15:16:19 +0100

    feat: activate close button click handler

--- js/utils/home/UIUtils.js
+++ js/utils/home/UIUtils.js
@@ -325,17 +325,17 @@ export function showPaymentContainerModal() {
   // click handlers
   modalContainer.addEventListener('click', showInitialPaymentModal)
 
-  //   document
-  //     .querySelectorAll('.button-modal-payment-close')
-  //     .forEach((buttonClose) => {
-  //       buttonClose.addEventListener('click', () => {
-  //         // LocalStorageManager.save(IS_READY_TO_PAYMENT, false)
-  //         hideInitialPaymentModal()
-  //         hideFinalPaymentModal()
-  //         hidePaymentContainerModal()
-  //         // modalContainer.removeEventListener('click')
-  //       })
-  //     })
+  document
+    .querySelectorAll('.button-modal-payment-close')
+    .forEach((buttonClose) => {
+      buttonClose.addEventListener('click', () => {
+        // LocalStorageManager.save(IS_READY_TO_PAYMENT, false)
+        hideInitialPaymentModal()
+        hideFinalPaymentModal()
+        hidePaymentContainerModal()
+        modalContainer.removeEventListener('click', modal-payment-container)
+      })
+    })
 }
 
 export function showInitialPaymentModal() {
