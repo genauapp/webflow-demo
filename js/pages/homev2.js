37ca8e8f8ef8301e2d14f3fce9a61b9db78c8e29
unknown <chatonode@gmail.com>
2025-05-13 20:07:52 +0100

    fix: importing named export

--- js/pages/homev2.js
+++ js/pages/homev2.js
@@ -1,4 +1,4 @@
-import initializeGoogleAuth from '../service/oauth2/googleAuthService.js'
+import { initializeGoogleAuth } from '../service/oauth2/googleAuthService.js'
 
 document.getElementById('google-signin').addEventListener('click', async () => {
   console.log('YAYYYYYYYYYYYYYYYY! Clicked!')
