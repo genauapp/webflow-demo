2c0017016d1559646d8d5aa8fc369ba0b35c2f88
unknown <chatonode@gmail.com>
2025-05-17 14:27:05 +0100

    feat: update api server base url

--- js/constants/urls.js
+++ js/constants/urls.js
@@ -3,4 +3,5 @@ export const ASSETS_BASE_URL = `https://genauapp.github.io/webflow-demo/`
 // STAGING
 // export const ASSETS_BASE_URL = 'https://cdn.jsdelivr.net/gh/genauapp/webflow-demo@v8dcf59f'
 
-export const API_SERVER_BASE_URL = 'https://genauapp-wbf-api.site'
+// export const API_SERVER_BASE_URL = 'https://genauapp-wbf-api.site'
+export const API_SERVER_BASE_URL = 'https://api.genauapp.io'
