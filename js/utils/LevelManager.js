c3b902f77c1394409432c66b78e9a6bc1553e859
s2jack <gercekselcukguler@gmail.com>
2025-05-27 16:30:04 +0300

    fix syntax errors

--- js/utils/LevelManager.js
+++ js/utils/LevelManager.js
@@ -1,25 +1,22 @@
 import { CURRENT_CATEGORY_KEY } from "../constants/storageKeys.js";
 import LocalStorageManager from "./LocalStorageManager.js";
+import { categories } from "../constants/props.js";
 
 export default class LevelManager {
     static getCurrentLevel() {
         const pathSegments = window.location.pathname.split('/');
         const level = pathSegments.includes('level') ? pathSegments[pathSegments.length - 1] : null;
-
-        console.log('current level is ' + level); // "a1" ya da null
         return level
     }
 
     static getCurrentCategory() {
         const category = LocalStorageManager.load(CURRENT_CATEGORY_KEY)
-        console.log('current category is ' + category);
-
         return category
     }
 
     static checkIfCategoryIsInCategories(category) {
-        const categories = categories[this.getCurrentLevel()]
-        return categories.some(cat => cat.nameShort === category)
+        const levelCategories = categories[this.getCurrentLevel()]
+        return levelCategories.some(cat => cat.nameShort === category)
     }
 
 }
