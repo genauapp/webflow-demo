1421c067f07987801fb2b47a5229e12c989f2a1b
s2jack <gercekselcukguler@gmail.com>
2025-06-05 18:52:31 +0300

    change base url

--- js/constants/urls.js
+++ js/constants/urls.js
@@ -1,6 +1,6 @@
 // PROD
 //export const ASSETS_BASE_URL = `https://genauapp.github.io/webflow-demo/`
 // STAGING
-export const ASSETS_BASE_URL = 'https://cdn.jsdelivr.net/gh/genauapp/webflow-demo@39679dc'
+export const ASSETS_BASE_URL = 'https://cdn.jsdelivr.net/gh/genauapp/webflow-demo@b3fb811'
 
 export const API_SERVER_BASE_URL = 'https://api.genauapp.io'
