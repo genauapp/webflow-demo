60a94b5f04e831120906e2735bf015dced2423ef
Cevdet Çağdaş Köse <chatonode@gmail.com>
2025-06-06 16:40:45 +0100

    feat: event counter manager for signin event modal

---
+++ js/constants/events.js
@@ -0,0 +1,8 @@
+// /constants/events.js
+export const SigninModalTriggerEvents = Object.freeze({
+  HOME_SEARCH_ADD_TO_BOOKMARKS: 'home:search-add-to-bookmarks',
+  LEVEL_LEARN_ADD_TO_BOOKMARKS: 'level:learn-add-to-bookmarks',
+  LEVEL_LEARN_FINISH: 'level:learn-finish',
+  LEVEL_EXERCISE_FINISH: 'level:exercise-finish',
+  EINBURGERUNGSTEST_TEST_FINISH: 'einburgerungstest:test-finish',
+})
