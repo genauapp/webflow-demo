6594878ca38df27f2ce07b3436d0b60d973f0f87
Cevdet Çağdaş Köse <chatonode@gmail.com>
2025-06-08 01:55:28 +0100

    fix: creating script order

--- js/service/oauth2/googleAuthService.js
+++ js/service/oauth2/googleAuthService.js
@@ -1,23 +1,7 @@
 import { GOOGLE_CLIENT_ID_WEB } from '../../constants/auth/google.js'
 
 export async function initGoogleAuth(buttonId, onSuccess) {
-  window.onGoogleLibraryLoad = () => {
-    // 1) Configure the client
-    google.accounts.id.initialize({
-      client_id: GOOGLE_CLIENT_ID_WEB,
-      callback: handleCredentialResponse,
-      context: 'use', // optional: ensures one‑tap context
-      ux_mode: 'popup', // popup vs. redirect
-    })
-
-    // 2) Render the real button into your container
-    google.accounts.id.renderButton(document.getElementById(buttonId), {
-      theme: 'outline',
-      size: 'large',
-    })
-  }
-
-  // 3) Load the library (ensures onGoogleLibraryLoad is called)
+  // 1) Load the library (ensures onGoogleLibraryLoad is called)
   const script = document.createElement('script')
   script.src = 'https://accounts.google.com/gsi/client'
   script.async = true
@@ -25,9 +9,25 @@ export async function initGoogleAuth(buttonId, onSuccess) {
   script.onload = () => window.onGoogleLibraryLoad()
   document.head.appendChild(script)
 
+  window.onGoogleLibraryLoad = () => {
+    // 2) Configure the client
+    google.accounts.id.initialize({
+      client_id: GOOGLE_CLIENT_ID_WEB,
+      callback: handleCredentialResponse,
+      context: 'use', // optional: ensures one‑tap context
+      ux_mode: 'popup', // popup vs. redirect
+    })
+
+    // 3) Render the real button into your container
+    google.accounts.id.renderButton(document.getElementById(buttonId), {
+      theme: 'outline',
+      size: 'large',
+    })
+  }
+
   async function handleCredentialResponse(googleResponse) {
     const idToken = googleResponse.credential // JWT from Google
     // console.log(`google id_token: ${idToken}`)
-    onSuccess(idToken)
+    await onSuccess(idToken)
   }
 }
