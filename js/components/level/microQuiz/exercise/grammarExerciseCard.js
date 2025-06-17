52ed28637db4f172ca20ee700bb1eca0a2a4d907
Cevdet Çağdaş Köse <chatonode@gmail.com>
2025-06-18 00:05:45 +0100

    revert: list utils shuffle

--- js/components/level/microQuiz/exercise/grammarExerciseCard.js
+++ js/components/level/microQuiz/exercise/grammarExerciseCard.js
@@ -67,9 +67,12 @@ function generateGrammarOptions(correctWord, allWords, optionsCount) {
   }
 
   // Shuffle the options
-  const shuffledOptions = ListUtils.shuffleArray(options)
+  for (let i = options.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1))
+    ;[options[i], options[j]] = [options[j], options[i]]
+  }
 
-  return shuffledOptions
+  return options
 }
 
 /** NEW: Render grammar exercise options */
