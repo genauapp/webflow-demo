5fc1176b379bc2cf6fa4979f2df38f832b69992b
Cevdet Çağdaş Köse <chatonode@gmail.com>
2025-06-18 12:23:13 +0100

    feat: latest assets base url because of json change

--- js/constants/urls.js
+++ js/constants/urls.js
@@ -1,6 +1,6 @@
 // PROD
 //export const ASSETS_BASE_URL = `https://genauapp.github.io/webflow-demo/`
 // STAGING
-export const ASSETS_BASE_URL = 'https://cdn.jsdelivr.net/gh/genauapp/webflow-demo@92d8998'
+export const ASSETS_BASE_URL = 'https://cdn.jsdelivr.net/gh/genauapp/webflow-demo@0b3054e'
 
 export const API_SERVER_BASE_URL = 'https://api.genauapp.io'
