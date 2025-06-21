eaf44922ebc7317fc4c2d99e746cdea089920ce6
Cevdet Çağdaş Köse <chatonode@gmail.com>
2025-06-21 16:39:41 +0100

    feat: update assets base url

--- js/constants/urls.js
+++ js/constants/urls.js
@@ -2,7 +2,7 @@
 //export const ASSETS_BASE_URL = `https://genauapp.github.io/webflow-demo/`
 // STAGING
 export const ASSETS_BASE_URL =
-  'https://cdn.jsdelivr.net/gh/genauapp/webflow-demo@1586399'
+  'https://cdn.jsdelivr.net/gh/genauapp/webflow-demo@8eee61c'
 
 export const CDN_BASE_URL = 'https://genauapp.github.io/cdn-temp'
 
