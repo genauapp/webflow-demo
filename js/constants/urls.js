5323c53f6ddab71071b2f5de3ed8aeb257f0a00f
s2jack <gercekselcukguler@gmail.com>
2025-07-08 19:00:59 +0300

    update assets base url

--- js/constants/urls.js
+++ js/constants/urls.js
@@ -2,7 +2,7 @@
 // export const ASSETS_BASE_URL = `https://genauapp.github.io/webflow-demo/`
 // STAGING
 export const ASSETS_BASE_URL =
-  'https://cdn.jsdelivr.net/gh/genauapp/webflow-demo@8ee10ed'
+  'https://cdn.jsdelivr.net/gh/genauapp/webflow-demo@4d71f18'
 
 export const CDN_BASE_URL = 'https://genauapp.github.io/cdn-temp'
 
