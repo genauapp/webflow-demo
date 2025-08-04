e4f039b5a3c0052070dd884e7f4243c678d054ab
Cevdet Çağdaş Köse <chatonode@gmail.com>
2025-08-04 22:41:30 +0100

    feat: update staging api base url

--- js/constants/urls.js
+++ js/constants/urls.js
@@ -9,4 +9,4 @@ export const CDN_BASE_URL = 'https://genauapp.github.io/cdn-temp'
 // PROD
 // export const API_SERVER_BASE_URL = 'https://api.genauapp.io'
 // STAGING
-export const API_SERVER_BASE_URL = 'https://73645ae35adb.ngrok-free.app'
+export const API_SERVER_BASE_URL = 'https://7b19b918d822.ngrok-free.app'
