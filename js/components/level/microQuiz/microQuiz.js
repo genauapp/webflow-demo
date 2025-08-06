aabe770c35bc8bf91907ad736cc5622c453b1ddf
Cevdet Çağdaş Köse <chatonode@gmail.com>
2025-08-06 02:30:01 +0100

    fix: send micro-quiz deck summary while mounting deckPractice

--- js/components/level/microQuiz/microQuiz.js
+++ js/components/level/microQuiz/microQuiz.js
@@ -8,7 +8,15 @@ export function mountMicroQuiz(packSummary) {
   // Implementation similar to mountPackJourney but for micro-quiz
   const firstDeckSummary = packSummary.deck_summaries[0]
 
-  mountDeckPractice(firstDeckSummary)
+  mountDeckPractice({
+    id: firstDeckSummary.deck_id,
+    wordType: firstDeckSummary.word_type,
+    exerciseType: firstDeckSummary.exercise_type,
+    createdAt: firstDeckSummary.created_at,
+    updatedAt: firstDeckSummary.updated_at,
+    wordsCount: firstDeckSummary.words_count,
+    status: firstDeckSummary.user_deck_status,
+  })
 }
 
 export function unmountMicroQuiz() {
