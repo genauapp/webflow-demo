f5c3e6863e7c1cb7c961f53c0830734a1c2571c3
Cevdet Çağdaş Köse <chatonode@gmail.com>
2025-08-28 16:58:22 +0300

    fix: total words count for each pack in correct level page component

--- js/pages/level.js
+++ js/pages/level.js
@@ -171,6 +171,18 @@ function loadPackPropsOnLevelPage(packSummariesOfCurrentLevel) {
     linkBlock.appendChild(img)
     linkBlock.appendChild(h1)
 
+    // Add pack-summary-words-count-container element
+    const wordsCount = pack.total_words_count || 0
+    const wordsCountHTML = `
+    <div id="Word_Count_Badge" class="w-layout-hflex pack-summary-words-count-container">
+      <img src="https://cdn.prod.website-files.com/677da6ae8464f53ea15d73ac/683c7cefa0f6a98cfe516c78_WordsIcon.svg" loading="lazy" alt="" class="image-21">
+      <div class="w-layout-hflex pack-summary-words-count">
+        <div id="Word_Count_Text" class="words-count-label">${wordsCount}</div>
+      </div>
+    </div>
+  `
+    packEl.insertAdjacentHTML('beforeend', wordsCountHTML)
+
     // Son olarak istediğin yere ekle, örneğin bir container'a:
     if (packSummary.pack_type === PackType.JOURNEY) {
       journeyPackSummaryGrid.appendChild(linkBlock)
