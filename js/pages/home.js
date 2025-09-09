89715aaa592e783bea13e598d907084ffd7d25c9
Cevdet Çağdaş Köse <chatonode@gmail.com>
2025-09-09 16:18:44 +0300

    refactor: split translate and home page components within separate sub-modules

--- js/pages/home.js
+++ js/pages/home.js
@@ -1,42 +1,9 @@
 // /pages/home.js
-import { initTranslationComponent } from '../components/home/translation.js'
 import { initLevelComponent } from '../components/home/level.js'
 import { mountStarterPack } from '../components/home/starterPack.js'
-import LocalStorageManager from '../utils/LocalStorageManager.js'
 
 // Element IDs are kept in the page file
 const elementIds = {
-  // Search component elements
-  search: {
-    form: 'form-search',
-    input: 'input-search',
-    inputSuggestionsContainer: 'search-input-suggestions-container',
-    inputCloseButton: 'btn-search-input-close',
-    // button: 'search-button',
-    loading: 'search-loading-container',
-    // error: 'search-error',
-    emptyInputContainer: 'search-empty-input-container',
-    noResultsContainer: 'search-no-results-container',
-    results: {
-      container: 'search-results-container',
-      levelBadge: 'word-card-level-badge',
-      typeBadge: 'word-card-type-badge',
-      title: 'word-card-title',
-      translation: 'word-card-translation',
-      ruleContainer: 'word-card-rule-container',
-      rule: 'word-card-rule',
-      sentence: 'word-card-sentence',
-      verb: {
-        caseLabelsContainer: 'word-card-verb-case-labels-container',
-        caseLabel: (verbCase) => `${verbCase}-label`,
-        caseDetailsContainer: (verbCase) => `${verbCase}-details-container`,
-      },
-      addToBookmarksButton: 'btn-search-add-to-bookmarks',
-      labelRequiresSignin: 'label-requires-signin',
-      ttsPlayButton: 'btn-search-tts-play',
-    },
-  },
-
   level: {
     // Level component elements
     levelA1Btn: 'level-a1-btn',
@@ -50,10 +17,9 @@ const elementIds = {
 async function bootstrap() {
   LocalStorageManager.clearDeprecatedLocalStorageItems()
 
-  // Initialize both components with their respective element IDs
-  initTranslationComponent({ ...elementIds.search })
   // initLevelComponent({ ...elementIds.level })
-  
+  // Initialize both components with their respective element IDs
+
   // Check if starter pack is completed
   const isCompleted = LocalStorageManager.load('STARTER_PACK_COMPLETED', false)
   if (!isCompleted) {
